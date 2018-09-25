ef4e812597f8e321c8e35835711cf43855e7de9c
Оборин Роман <roma@alpom.ru>
2018-09-25 23:56:40 +0300

    Lab 8

---
+++ lab8Source.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#define MAX 1000
+
+
+bool isLettersRepeating(const char *firstLetter, const char *secondLetter);
+/*В символьной строке оставить только те  слова, в которых встречаются подряд идущие одинаковые буквы*/
+int  main(){
+	system("chcp 1251");
+
+
+	char line[MAX];
+	int found = 0;
+	printf("Введите строку\n");
+	fgets(line, MAX,stdin);
+
+	char *firstLetter;                                     
+	char *secondLetter;                                     
+	char *space;                                   
+	firstLetter = &line[0];
+	secondLetter = &line[1];
+	space = firstLetter ;                             
+	
+
+	do                                              
+	{
+		secondLetter++;
+		firstLetter++;
+		if (isLettersRepeating(firstLetter,secondLetter))                     
+		{
+			found = 1;                             
+		}
+		if ((*secondLetter == ' ') || (*secondLetter == '\0'))       
+
+		{
+
+			if (found == 0)                            
+			{
+				firstLetter = space;                        
+				while ((*space++ = *secondLetter++) != '\0');
+				secondLetter = firstLetter + 1;                     
+				space = firstLetter;
+			}
+			else
+			{
+				space = secondLetter;
+				found = 0;
+			}
+		}
+		
+	} while (*firstLetter != '\0');
+	printf("Строка со словами без повторяющихся букв\n");
+	puts(line);                                 //вывод результата
+	system("pause");	
+}
+bool isLettersRepeating(const char *firstLetter, const char *secondLetter) {
+	if ((*firstLetter == *secondLetter) && (((*firstLetter >= 'A')
+		&& (*firstLetter <= 'Z')) || ((*firstLetter >= 'a')
+		&& (*firstLetter <= 'z') || ((*firstLetter >= 'А')
+		&& (*firstLetter <= 'Я') || ((*firstLetter >= 'а')
+		&& (*firstLetter <= 'я')))))) {
+		return true;
+	}
+	return false;
+}
